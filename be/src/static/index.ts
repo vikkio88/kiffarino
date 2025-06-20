9f871524bba1ff659da3e3781a9cb9df89d4c2f8
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-06-20 20:33:59 +0100

    removed magic string

--- be/src/static/index.ts
+++ be/src/static/index.ts
@@ -2,8 +2,10 @@ import { type Context } from "hono";
 import { serveStatic } from "hono/serve-static";
 import fs from "node:fs";
 
+const STATIC_FILES_FOLDER = "./public/";
+
 const staticHandler = serveStatic({
-  root: "./public/",
+  root: STATIC_FILES_FOLDER,
   getContent: async (path, c: Context) => {
     //TODO: investigate if I can send a stream
     if (!fs.existsSync(path)) {
@@ -14,6 +16,5 @@ const staticHandler = serveStatic({
     return fs.readFileSync(path);
   },
 });
-
 
 export default staticHandler;
