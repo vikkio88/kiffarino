b587c984e7bde711db6aded641c39ca98f627dd5
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-06-22 15:29:37 +0100

    removed magic strings

--- shared/config/index.ts
+++ shared/config/index.ts
@@ -2,3 +2,5 @@ export const NAME = "Kiffarino";
 export const SERVER_PORT = 3003;
 export const EXEC_NAME = "kfr";
 export const PROJECT_CONFIG_FILENAME = ".kfrc";
+export const TICKETS_SUBFOLDER = "tickets";
+export const DOCS_SUBFOLDER = "docs";
