9ba3fd41948ec75d76bf0c89836299f340711691
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-06-24 19:31:14 +0100

    created loader

---
+++ web/src/components/shared/Spinner.svelte
@@ -0,0 +1,14 @@
+<div class="f rc">
+  <div class="spinner"></div>
+</div>
+
+<style>
+  .spinner {
+    border: 8px solid var(--accent-color);
+    border-top: 8px solid var(--accent-faint-color);
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    animation: spin 0.5s linear infinite;
+  }
+</style>
