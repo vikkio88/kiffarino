01898683943233366bad095aa156e4d178b16aa1
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-06-24 20:46:03 +0100

    fixed SPA serving

--- be/src/static/index.ts
+++ be/src/static/index.ts
@@ -3,12 +3,17 @@ import { serveStatic } from "hono/serve-static";
 import fs from "node:fs";
 
 const STATIC_FILES_FOLDER = "./public/";
+const INDEX_PATH = `public/index.html`;
 
 const staticHandler = serveStatic({
   root: STATIC_FILES_FOLDER,
   getContent: async (path, c: Context) => {
     //TODO: investigate if I can send a stream
     if (!fs.existsSync(path)) {
+      const isFile = /\.[^\/]+$/.test(path);
+      if (isFile) {
+        return fs.readFileSync(INDEX_PATH);
+      }
       c.status(404);
       return c.text("Not Found");
     }
