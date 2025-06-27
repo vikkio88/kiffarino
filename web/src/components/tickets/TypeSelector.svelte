bdb5c9bb3407cd9119a3d60850420da5ada787ae
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-06-27 19:36:05 +0100

    creating type selector

---
+++ web/src/components/tickets/TypeSelector.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { ticketTypes, type TicketType } from "@kiffarino/shared";
+  import { typeEmojiMap, typeLabelMap } from "./type";
+
+  type Props = {
+    type: TicketType;
+    onChange?: (type: TicketType) => void;
+  };
+
+  const { type, onChange = console.log }: Props = $props();
+  let selectedType = $state(type);
+
+  const handleChange = (e: Event) => {
+    const newType = (e.target as HTMLSelectElement).value as TicketType;
+    selectedType = newType;
+    onChange(newType);
+  };
+</script>
+
+<div class="f r g ai-center">
+  <span>{typeEmojiMap[selectedType]}</span>
+  <select class="n-select" onchange={handleChange} bind:value={selectedType}>
+    {#each ticketTypes as t}
+      <option value={t}>
+        {typeLabelMap[t]}
+      </option>
+    {/each}
+  </select>
+</div>
