d8681e3c512453c6f56dbda09085e1cdd71c1fa4
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-06-30 19:45:27 +0100

    adding about in home

--- web/src/pages/Home.svelte
+++ web/src/pages/Home.svelte
@@ -3,3 +3,31 @@
 </script>
 
 <Board />
+
+<div class="bottom">
+  <span>
+    <a href="https://github.com/vikkio88/kiffarino" target="_blank">
+      🧑‍💻 kiffarino
+    </a>
+  </span>
+</div>
+
+<style>
+  .bottom {
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 1rem;
+    bottom: 0;
+    width: 100%;
+    font-size: small;
+  }
+  .bottom > span > a {
+    color: var(--gray-1-color);
+  }
+
+  .bottom > span > a:hover {
+    color: var(--primary-color);
+  }
+</style>
