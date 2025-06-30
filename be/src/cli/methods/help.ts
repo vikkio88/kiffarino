64f3c8e1b3c1d96d8992ea66f80d188336d9d45c
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-06-30 19:43:23 +0100

    improved help

--- be/src/cli/methods/help.ts
+++ be/src/cli/methods/help.ts
@@ -4,11 +4,15 @@ export function help() {
   console.log(`
 ${NAME} — your local project management tool
 
+https://github.com/vikkio88/kiffarino
+
 Available commands:
 
   init         Create a ${PROJECT_CONFIG_FILENAME} config file in the current directory
   generate     Scaffold the project folder defined in ${PROJECT_CONFIG_FILENAME} (e.g. config, tickets/)
   start        Launch the local web UI to browse and manage your tickets
+  
+  version      print the \`kfr\` cli version.
 
 Usage:
 
