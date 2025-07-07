7d088eef6874c9af0d300b92df7a5a2d518330c3
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-07-07 21:47:27 +0100

    more work on md editor

--- web/src/components/shared/MdEditor.svelte
+++ web/src/components/shared/MdEditor.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+  import { tick } from "svelte";
+
+  const INSERT_PATTERN = "%SELECTION%";
+  const CURSOR_POSITION = "%URL%";
+
   type Props = {
     initialValue?: string;
     placeholder?: string;
@@ -10,12 +15,61 @@
     placeholder = "Add a description...",
     onChange,
   }: Props = $props();
+  let textarea: HTMLTextAreaElement;
   let value = $state(initialValue ?? "");
+
+  async function insertAtSelection(
+    pattern: string,
+    fallback: string | undefined = undefined
+  ) {
+    let selected = value.slice(textarea.selectionStart, textarea.selectionEnd);
+    if (selected === "" && !fallback) {
+      return;
+    }
+
+    if (selected === "" && fallback) {
+      selected = fallback;
+    }
+
+    const newValue = `${value.slice(0, textarea.selectionStart)}${pattern.replace(INSERT_PATTERN, selected).replace(CURSOR_POSITION, "URL")}${value.slice(textarea.selectionEnd)}`;
+
+    value = newValue;
+    await tick();
+    const cursorPos = newValue.indexOf("URL");
+
+    textarea.focus();
+    textarea.setSelectionRange(cursorPos, cursorPos + 3);
+  }
+
+  const addImage = () => {
+    insertAtSelection(`\n![${INSERT_PATTERN}](${CURSOR_POSITION})\n`, "Image Alt");
+  };
+
+  const addLink = () => {
+    insertAtSelection(`[${INSERT_PATTERN}](${CURSOR_POSITION})`);
+  };
 </script>
 
-<textarea onchange={() => onChange(value)} bind:value {placeholder}></textarea>
+<div class="commands">
+  <button class="n-btn" onclick={addImage}>🖼️</button>
+  <button class="n-btn" onclick={addLink}>🔗</button>
+</div>
+<textarea
+  bind:this={textarea}
+  onchange={() => onChange(value)}
+  bind:value
+  {placeholder}
+></textarea>
 
 <style>
+  .commands {
+    width: 100%;
+    background-color: var(--gray-1-color);
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+    padding: 0.5rem 1rem;
+  }
   textarea {
     width: 100%;
     height: 90%;
