52bb7eedd3e11dec16b60403522d9fc3a29961b1
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-07-07 19:25:24 +0100

    adding way to get url param

--- web/src/libs/routing.ts
+++ web/src/libs/routing.ts
@@ -6,9 +6,23 @@ export type RouteParams<T> = {
   };
 };
 
+export type QueryParams<T> = {
+  result: {
+    querystring: {
+      params: T;
+    };
+  };
+};
+
 export function getParam<T extends object, K extends keyof T>(
   route: RouteParams<T>,
   key: K
 ): T[K] | undefined {
   return route.result.path.params[key];
+}
+export function getQueryParam<T extends object, K extends keyof T>(
+  route: QueryParams<T>,
+  key: K
+): T[K] | undefined {
+  return route.result.querystring.params[key];
 }
